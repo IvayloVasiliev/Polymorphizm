58b380d4d667fce3418acc340fd04b9114ed57c8
Ivailo Vasiliev <ivailo_vasiliev@hotmail.com>
2019-12-23 22:04:33 +0200

    Add project files.

---
+++ Vehicles/Vehicles/Car.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vehicles.Vehicles
+{
+    public class Car : Vehicle
+    {
+        private const double airConditionerConsumption = 0.9;
+        public Car(double fuelQuantity, double fuelConsumption, double tankCapacity)
+            : base(fuelQuantity, fuelConsumption, tankCapacity)
+        {
+            this.FuelConsumption += airConditionerConsumption;
+        }
+    }
+}
---
+++ Vehicles/Vehicles/Truck.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vehicles.Vehicles
+{
+    public class Truck : Vehicle
+    {
+        private const double airConditionerConsumption = 1.6;
+        public Truck(double fuelQuantity, double fuelConsumption, double tankCapacity) 
+            : base(fuelQuantity, fuelConsumption, tankCapacity)
+        {
+            this.FuelConsumption += airConditionerConsumption;
+
+        }
+
+        public override void Refuel(double fuel)
+        {
+            if (fuel <= 0)
+            {
+                throw new ArgumentException("Fuel must be a positive number");
+            }
+
+            if (FuelQuantity + fuel > TankCapacity)
+            {
+                throw new ArgumentException($"Cannot fit {fuel} fuel in the tank");
+            }
+
+            FuelQuantity += fuel * 0.95;
+        }
+    }
+}
